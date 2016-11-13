c426e6e25ad416c1e30ad30848cf77457f79da8e
chinduvv <chinduvallath@gmail.com>
2016-11-13 15:02:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,5 @@
 console.log('Loaded!');
+function show() {
+            //document.getElementById("dThreshold").display ="block";
+            document.getElementById("dThreshold").style.display = "block";
+        }
