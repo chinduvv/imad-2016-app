2dc3dc26502c85aa5ea5986270569642e37c3bd5
chinduvv <chinduvallath@gmail.com>
2016-11-18 10:36:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
@@ -21,4 +21,138 @@ app.get('/ui/madi.png', function (req, res) {
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
+});
+*/
+var express=require('express');
+var morgan=require('morgan');
+var path=require('path');
+var app=express();
+app.use(morgan('combined'));
+
+var articles = {
+var article-one:{
+title: 'Article One| Chindu',
+heading: 'Article One',
+date: 'Sep 5,2016',
+content:` 
+<p>
+ferferfgfhjjhhjhbhbdhjsdbhfffhegfjew
+fhjjjjjjjjjjjjjjj
+hjfhjhkfhkhkjhkhhkhkhkhk
+</p>
+<p>
+efergrfr rgbjhgrtjg fhrjgfrg gerf
+febr rfrg4 jhfregrekfgrtgrg grgrtgtr gtgrtg 
+trgrtgrtg grgr 
+</p>
+<p>
+rgrtgtg rtgrtg g556
+fhwefrwg4gtnf hbgugkg4
+fgge
+ghjgjg
+</p> `
+},
+article-two:{
+title: 'Article One| Chindu',
+heading: 'Article One',
+date: 'Sep 5,2016',
+content:` 
+<p>
+ferferfgfhjjhhjhbhbdhjsdbhfffhegfjew
+fhjjjjjjjjjjjjjjj
+hjfhjhkfhkhkjhkhhkhkhkhk
+</p>
+<p>
+efergrfr rgbjhgrtjg fhrjgfrg gerf
+febr rfrg4 jhfregrekfgrtgrg grgrtgtr gtgrtg 
+trgrtgrtg grgr 
+</p>
+<p>
+rgrtgtg rtgrtg g556
+fhwefrwg4gtnf hbgugkg4
+fgge
+ghjgjg
+</p>
+},
+article-three:{
+title: 'Article One| Chindu',
+heading: 'Article One',
+date: 'Sep 5,2016',
+content:` 
+<p>
+ferferfgfhjjhhjhbhbdhjsdbhfffhegfjew
+fhjjjjjjjjjjjjjjj
+hjfhjhkfhkhkjhkhhkhkhkhk
+</p>
+<p>
+efergrfr rgbjhgrtjg fhrjgfrg gerf
+febr rfrg4 jhfregrekfgrtgrg grgrtgtr gtgrtg 
+trgrtgrtg grgr 
+</p>
+<p>
+rgrtgtg rtgrtg g556
+fhwefrwg4gtnf hbgugkg4
+fgge
+ghjgjg
+</p>
+}
+};
+function createTemplate (data){
+var title= data.title;
+var date= data.date;
+var heading= data.heading;
+var content= data.content;
+
+var htmlTemplate=`
+<html>
+<head>
+<title>
+${title}
+</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link href="/ui/style.css" rel="stylesheet"/>
+</head>
+<body>
+<div class="container">
+<div>
+<a href="/">Home</a>
+</div>
+<hr/>
+<h3>
+${heading}
+</h3>
+<div>
+${date}
+</div>
+<div>
+${content}
+</div>
+</div>
+</body>
+</html>
+
+ 
+
+`
+;
+return htmlTemplate;
+}
+
+app.get('/',function(req,res){
+res.sendFile(path.join(__dirname,'ui',index.html'));
+});
+app.get('/:articleName',function(req,res){
+var articleName= req.params.articleName;
+res.send(createTemplate(articles[articleName]));
+});
+app.get('/article-two',function(req,res){
+res.sendFile(path.join(__dirname,'ui',style.css'));
+});
+app.get('/ui/madi.png',function(req,res){
+res.sendFile(path.join(__dirname,'ui',madi.png'));
+});
+
+var port=8080
+app.listen(8080,function(){
+console.log('IMAD course app listening on port S{port}!');
 });
